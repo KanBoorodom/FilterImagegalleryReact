74c4648be92a74e8eb12c5620431d7e2fec60e70
Kan <kan.boorodom@gmail.com>
2021-07-09 14:45:59 +0700

    'edit'

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React</h1>
+        <h1>Kan's Filter image gallery</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
